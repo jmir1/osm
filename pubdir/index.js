64716884ac3a0e83b0fc5cffb8932b50b7565616
jmir1 <jhmiramon@gmail.com>
2021-04-01 01:38:55 +0200

    no google please

--- pubdir/index.js
+++ pubdir/index.js
@@ -3,7 +3,7 @@ var get_ranking = new XMLHttpRequest();
 get_ranking.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         result = get_ranking.responseText;
-        rankingdiv.innerHTML = '<pre class="prettyprint">' + result + '</pre>';
+        rankingdiv.innerHTML = '<pre style="border: 1px solid gray; padding: 2px;">' + result + '</pre>';
     }
 };
 get_ranking.open("GET", "/rankings", true);
